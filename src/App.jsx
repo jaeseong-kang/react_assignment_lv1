b4620e8b69cd2ce7e2289a558bd8f50881610ba0
jaeseong-kang <kg9884@naver.com>
2023-05-22 14:46:01 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='headline'>
+        <span>My Todo List</span>
+        <span>React</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
